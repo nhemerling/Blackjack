9c4d6023490e1c441c4540180bb4e7eb2010ea9e
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2021-01-29 10:09:40 -0800

    add Hand methods and try them out in Main

--- src/com/thinkful/blackjack/Hand.java
+++ src/com/thinkful/blackjack/Hand.java
@@ -15,4 +15,32 @@ public class Hand {
         this.cards = new ArrayList<>();
     }
 
+    public void addCard(Card card) {
+        this.getCards().add(card);
+    }
+
+    public int getValue() {
+        int total = 0;
+        for(Card card: this.getCards()) {
+            total += card.getValue();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[");
+        for (Card c: this.getCards()) {
+            sb.append(String.format("%s", c));
+        }
+        if (this.getCards().size() > 0) {
+            sb.deleteCharAt(sb.length() - 1); // remove the extraneous comma
+        }
+        sb.append("]");
+
+        return String.format("Value=%d, %s", this.getValue(), sb.toString());
+    }
+
 }
